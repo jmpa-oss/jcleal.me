236404652f575a9b416840ed54594eaf9f01c840
Jordan Cleal <mr.jordan.cleal+github@gmail.com>
2020-12-23 16:27:19 +1100

    implement sync + deployment mech to deploy the static site

--- bin/30-sync.sh
+++ bin/30-sync.sh
@@ -4,8 +4,12 @@
 # funcs
 die() { echo "$1" >&2; exit "${2:-1}"; }
 
+# check project root
+[[ ! -d .git ]] \
+  && die "must be run from repository root directory"
+
 # check deps
-deps=(aws)
+deps=(docker aws)
 for dep in "${deps[@]}"; do
   hash "$dep" 2>/dev/null || missing+=("$dep")
 done
@@ -14,9 +18,36 @@ if [[ ${#missing[@]} -ne 0 ]]; then
   die "missing dep${s}: ${missing[*]}"
 fi
 
+# check path
+path="./public"
+[[ -d "$path" ]] \
+  || die "missing $path"
+
 # check auth
 aws sts get-caller-identity &>/dev/null \
   || die "unable to connect to AWS; are you authed?"
 
+# compile hugo site
+echo "##[group]Compiling static site"
+docker run --rm -it \
+  -w /app \
+  -v "$PWD:/app" \
+  klakegg/hugo:0.78.2-alpine \
+  || die "failed to compile hugo site"
+echo "##[endgroup]"
+
+# get stack name
+stack="$(basename "$PWD")" \
+  || die "failed to get repository name"
+stack="${stack/\./-}"
+
+# get bucket
+bucket=$(aws cloudformation describe-stacks --stack-name "$stack" \
+    --query 'Stacks[].Outputs[?OutputKey==`Bucket`].OutputValue' --output text) \
+    || die "failed to get bucket name for $stack"
+
 # sync to s3
-# TODO
+echo "##[group]Syncing to s3"
+aws s3 sync "$path" "s3://$bucket" \
+    || die "failed to sync $path to $bucket"
+echo "##[endgroup]"
